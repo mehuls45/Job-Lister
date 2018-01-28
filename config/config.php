7ece9ab5fafa7fff9d9e15895446c45cf6b7e388
Mehul <mehulsolanki45@gmail.com>
2018-01-28 09:45:27 +0530

    first commit
    Pushed files

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php 
+
+// DB Params
+define("DB_HOST","localhost");
+define("DB_USER","root");
+define("DB_PASS","1234");
+define("DB_NAME","joblister");
+
+define("SITE_TITLE","JobLister");
+?>
